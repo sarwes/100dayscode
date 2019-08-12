008968a76f56f1c2bfe999daf0e779d46beb65be
sarwes <32338003+sarwes@users.noreply.github.com>
2019-08-12 00:58:37 -0700

    twopoints

---
+++ day008.py
@@ -0,0 +1,18 @@
+import math
+# get the x coordinate of Point 1
+x1 = int(input())  
+# get the y coordinate of Point 2
+y1 = int(input())  
+# get the x coordinate of Point 2
+x2=int(input())
+# get the y coordinate of Point 2
+y2=int(input())
+x=x2-x1
+y=y2-y1
+z=((x*x)+(y*y))
+a=(math.sqrt(z))
+print(a)
+# Write the code to calculate distance between 
+# the two points 
+
+
